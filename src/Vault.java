9be0241d0cf3edcc4675968bf50feecb5aa49f68
League <League@AdminisorsiMac2.attlocal.net>
2018-06-01 18:28:06 -0700

    TeaParty, Vault IP

---
+++ src/Vault.java
@@ -0,0 +1,23 @@
+import java.util.Random;
+
+import javax.swing.JOptionPane;
+
+public class Vault {
+	boolean isRight;
+	int code;
+
+	public static void main(String[] args) {
+
+	}
+
+	public Vault(int code) {
+		this.code = code;
+	}
+
+	public void TryCode(int input) {
+		if (input == code) {
+
+		}
+	}
+
+}
